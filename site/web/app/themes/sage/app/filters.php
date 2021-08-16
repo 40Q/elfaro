f57b3f6aed4a154b76de9c6a26245c80d0c50b72
José Debuchy <jdebuchy@40q.com.ar>
2021-08-16 12:33:15 -0300

    Activate Google Maps API Key

--- site/web/app/themes/sage/app/filters.php
+++ site/web/app/themes/sage/app/filters.php
@@ -14,3 +14,11 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
+
+/**
+ * Include ACF API Key
+ */
+add_filter('acf/fields/google_map/api', function ($api) {
+    $api['key'] = $_ENV['GOOGLE_MAPS_API_KEY'];
+    return $api;
+});
